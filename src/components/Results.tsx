d330cd13e561467e07e51659f9696b26648b50b4
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2023-10-08 08:59:32 +0330

    feat: add useObservable

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react";
 import { SearchRepository, SearchResult } from "../store/store"
+import { useObservable } from "@ngneat/react-rxjs";
 
 export default function Results(){
-    const [result, setResult] = useState<SearchResult[] | null>(null);    
-    useEffect(()=>{
-        const repo = new SearchRepository();
-        repo.results$.subscribe((value)=> setResult(value))
-    },[])
+    const repo = new SearchRepository();
+    const [result] = useObservable(repo.results$);
     return (
         <>
             <div>
                 Hello from the <b>Results</b> component
             </div>
             {result?.map((value) => 
-                <div>
+                <div key={value.ID}>
                     <b>{value?.Title}</b><br></br>
                     {value?.Description}
                 </div>)}
